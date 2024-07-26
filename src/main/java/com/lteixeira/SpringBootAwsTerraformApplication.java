52899aa6a3570d5ed790a87dd945169172161f4b
LenilsonTeixeira <lenilsonts@gmail.com>
2024-07-26 02:05:12 -0300

    teste

--- src/main/java/com/lteixeira/SpringBootAwsTerraformApplication.java
+++ src/main/java/com/lteixeira/SpringBootAwsTerraformApplication.java
@@ -1,8 +1,10 @@
 package com.lteixeira;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class SpringBootAwsTerraformApplication {
 
+	@Value("${app.value}")
+	private String value;
+
 	@GetMapping
 	public String sayHello() {
 		return "Hello";
+	}
+
+	@GetMapping("/{name}")
+	public String sayHelloByName(@PathVariable("name") String name) {
+		return "Hello: "+ name + " By APP value= "+ value;
 	}
 
 	public static void main(String[] args) {
